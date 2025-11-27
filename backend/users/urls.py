7a89459e2dec7cdc81f767eec2fdc6a9f0b4ea3e
MichaBigler <micha.bigler2@gmail.com>
2025-11-27 09:54:50 +0100

    added debug view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from views_debug import WhoAmI
+from .views_debug import WhoAmI
 
 from .views import csrf_token_view, UserViewSet
 
