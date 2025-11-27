f7df2a4ec4930c724bed569f19563681ec23b932
MichaBigler <micha.bigler2@gmail.com>
2025-11-27 09:58:36 +0100

    added debug view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -12,7 +12,7 @@ router.register(r"", UserViewSet, basename="user")
 urlpatterns = [
     # nur noch CSRF-Helper + User-Viewset
     path("csrf/", csrf_token_view, name="csrf"),
-    path("api/debug/whoami/", WhoAmI.as_view()),
+    path("debug/whoami/", WhoAmI.as_view()), 
     
     path("", include(router.urls)),
 ]
